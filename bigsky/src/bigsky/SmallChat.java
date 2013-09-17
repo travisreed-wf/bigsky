d1ad02c4eacc91f3e6ca08e7977862f8a383c65b
Jonathan <Jonathan@Jonathan-PC>
2013-09-16 20:31:35 -0500

    Beggining of the small chat window!!!

---
+++ bigsky/src/bigsky/SmallChat.java
@@ -0,0 +1,31 @@
+package bigsky;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class SmallChat {
+	
+	public static void main(String[] args){
+		
+		JFrame jfrm = new JFrame("small chat");
+		jfrm.setSize(210,600);
+		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel jlab = new JLabel("TEST MESSAGE");
+		jfrm.add(jlab);
+		jfrm.setVisible(true);
+		jfrm.setLayout(new FlowLayout());
+		
+	}
+	
+	public SmallChat(){
+		
+		
+		
+	}
+	
+	
+	
+}
