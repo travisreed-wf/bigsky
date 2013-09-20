2e835656260877867dc0e8f1200ae6da4f813d25
Jonathan <Jonathan@Jonathan-PC>
2013-09-20 17:23:20 -0500

    it is telling me to commit

--- bigsky/src/bigsky/SmallChat.java
+++ bigsky/src/bigsky/SmallChat.java
@@ -1,31 +1,65 @@
 package bigsky;
 
-import java.awt.FlowLayout;
+import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import java.awt.Button;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.TextArea;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Canvas;
+import javax.swing.JTextArea;
 
 public class SmallChat {
-	
-	public static void main(String[] args){
+
+	private JFrame frmBluetext;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SmallChat window = new SmallChat();
+					window.frmBluetext.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public SmallChat() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmBluetext = new JFrame();
+		frmBluetext.setTitle("BlueText");
+		frmBluetext.getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 11));
+		frmBluetext.getContentPane().setBackground(Color.LIGHT_GRAY);
 		
-		JFrame jfrm = new JFrame("small chat");
-		jfrm.setSize(210,400);
-		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JTextArea messageArea = new JTextArea();
+		messageArea.setText("message...");
+		messageArea.setBounds(50,50, 50, 50);
+		frmBluetext.getContentPane();
 		
-		JLabel jlab = new JLabel("TEST MESSAGE");
-		jfrm.add(jlab);
-		jfrm.setVisible(true);
-		jfrm.setLayout(new FlowLayout());
+		
+		frmBluetext.setBounds(100, 100, 224, 315);
+		frmBluetext.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+
 		
 	}
-	
-	public SmallChat(){
-		
-		
-		
-	}
-	
-	
-	
+
 }
