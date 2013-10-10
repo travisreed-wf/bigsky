0ad7cccbb2f1d67fd9a8c4602cd27e40bd11b384
Andrew Hartman <hartman2@iastate.edu>
2013-10-10 01:06:30 -0500

    Semi done with login

--- bigsky/src/bigsky/Login.java
+++ bigsky/src/bigsky/Login.java
@@ -5,16 +5,24 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JPasswordField;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
-	private JPasswordField passwordField;
+	private JTextField textField;
 	private JPasswordField passwordField_1;
 
 	/**
@@ -45,10 +53,10 @@ public class Login extends JFrame {
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBackground(Color.GRAY);
-		passwordField.setBounds(111, 120, 130, 28);
-		contentPane.add(passwordField);
+		textField = new JTextField();
+		textField.setBackground(Color.GRAY);
+		textField.setBounds(111, 120, 130, 28);
+		contentPane.add(textField);
 		
 		passwordField_1 = new JPasswordField();
 		passwordField_1.setBounds(111, 160, 130, 28);
@@ -62,8 +70,141 @@ public class Login extends JFrame {
 		lblPassword.setBounds(17, 166, 98, 16);
 		contentPane.add(lblPassword);
 		
-		JButton btnNewButton = new JButton("Login");
-		btnNewButton.setBounds(139, 219, 70, 29);
-		contentPane.add(btnNewButton);
+		JButton login = new JButton("Login");
+		login.setBounds(139, 219, 70, 29);
+		contentPane.add(login);
+		
+		
+		login.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	            	
+            	try {
+					if(login()){
+						
+						System.out.println("Works");
+					}
+					else{
+						System.out.println("FAIL");
+					}
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+            	      
+            }
+        });
+
 	}
+	
+	public String getPassword(){
+	
+		String pass = "";
+		char [] password = passwordField_1.getPassword();
+		for(int i = 0; i < password.length; i++){
+			pass = pass + password[i];
+		}
+		return pass;	
+		
+	}
+	
+	public String getUsername(){
+		return  textField.getText();	
+	}
+	
+	//returns row number of users database row
+	public boolean login() throws ClassNotFoundException, SQLException{
+		
+		
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://mysql.cs.iastate.edu/db30901", "adm309", "EXbDqudt4");
+		Statement stmt = con.createStatement();
+		
+		//stmt.executeUpdate("insert into testTable (phoneNumber,lastName,firstName, password) values ('1111111111','Johnson','Missy','mypassword')");
+		ResultSet rs = con.createStatement().executeQuery("select * from testTable where phoneNumber='" + getUsername() + "'");
+	
+		if(rs.next() == false){
+			System.out.println("Username not found");
+			return false;
+			
+		}
+		
+		if(rs.getString("password") == null){
+			System.out.println("Password not found");
+			return false;			
+		}
+		
+		
+		System.out.println(getPassword());
+		System.out.println(getUsername());
+		System.out.println("-----------------");
+		System.out.println("-----------------");
+		System.out.println("-----------------");
+		System.out.println("-----------------");
+		System.out.println("-----------------");
+		System.out.println("-----------------");
+		System.out.println(rs.getString("password"));
+		System.out.println(rs.getString("phoneNumber"));
+		
+		
+		
+		
+		
+		
+		
+//		int count = 0;;
+//		rs.last();
+//		int numRows = rs.getRow();
+//		rs.first();
+//		for(int i =0; i <numRows; i++){
+//			count = 0;	
+//			if(rs.getString("phoneNumber") != null){
+//				System.out.println("Username Input is: " + getUsername());
+//				System.out.println("--------------------------------------------------");
+//				System.out.println("--------------------------------------------------");
+//				System.out.println("Username Database is: " + rs.getString("phonenumber"));
+//				System.out.println("--------------------------------------------------");
+//				System.out.println("--------------------------------------------------");
+//				if(getUsername().equals(rs.getString("phonenumber"))){
+//					count++;
+//				}
+//			}
+//			
+//			if(rs.getString("password") != null){
+//				System.out.println("Password Input is: " + getPassword());
+//				System.out.println("--------------------------------------------------");
+//				System.out.println("--------------------------------------------------");
+//				System.out.println("Password Database is: " + rs.getString("password"));
+//				System.out.println("--------------------------------------------------");
+//				System.out.println("--------------------------------------------------");
+//				if(getPassword().equals(rs.getString("password"))){
+//					count++;
+//				}
+//			}
+//				
+//			
+//			if(count == 2){
+//				return rs.getRow();
+//			}
+//			
+//			rs.next();
+//		}
+		
+		
+		rs.close();		
+		con.close();
+
+		return true;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 }
--- bigsky/src/bigsky/MySQLTest.java
+++ bigsky/src/bigsky/MySQLTest.java
@@ -3,6 +3,7 @@ package bigsky;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 
 public class MySQLTest {
@@ -14,12 +15,41 @@ public class MySQLTest {
 	public static void main(String[] args) throws Exception {
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection con = DriverManager.getConnection("jdbc:mysql://mysql.cs.iastate.edu/db30901", "adm309", "EXbDqudt4");
+		Statement stmt = con.createStatement();
 		
+		//stmt.executeUpdate("insert into testTable (phoneNumber,lastName,firstName, password) values ('1111111111','Johnson','Missy','mypassword')");
 		ResultSet rs = con.createStatement().executeQuery("select * from testTable");
-		//stmt.executeUpdate("insert into testTable (phoneNumber, password) values ('1231234567','mypassword')");
+	
+		rs.last();
+		int numRows = rs.getRow();
+		rs.first();
+		for(int i =0; i <numRows; i++){
+		
+		
+		
+		if(rs.getString("phoneNumber") != null){
+			System.out.println(rs.getString("phoneNumber"));
+		}
+		
+		if(rs.getString("lastName") != null){
+			System.out.println(rs.getString("lastName"));
+		}
+		
+		if(rs.getString("firstName") != null){
+			System.out.println(rs.getString("firstName"));
+		}
+		
+		if(rs.getString("password") != null){
+			System.out.println(rs.getString("password"));
+		}
+			
 		rs.next();
-		System.out.println(rs.getString("phoneNumber"));
-		System.out.println(rs.getString("password"));
+		System.out.println("--------------------");
+		}
+		
+		
+		
+		
 		rs.close();		
 		
 		con.close();
